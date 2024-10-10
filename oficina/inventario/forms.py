442d7983b9662902d3c4a0a9a4626ad895ad1ca9
ccampana <carlos.campana.d@gmail.com>
2024-10-10 11:19:02 -0300

    agrega boostrap a las cositas

--- oficina/inventario/forms.py
+++ oficina/inventario/forms.py
@@ -7,10 +7,21 @@ class EntregaForm(forms.ModelForm):
     class Meta:
         model = Entrega
         fields = ['insumo', 'cantidad_entregada', 'fecha_entrega', 'usuario']
+        widgets = {
+            'fecha_entrega': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'cantidad_entregada': forms.NumberInput(attrs={'class': 'form-control'}),
+            'insumo': forms.Select(attrs={'class': 'form-control'}),
+            'usuario': forms.Select(attrs={'class': 'form-control'}),
+        }
 
-    # Opcionalmente, puedes personalizar los widgets
-    # para el campo de usuario si lo necesitas
     usuario = forms.ModelChoiceField(
-        queryset=User.objects.all().exclude(pk=1),  # Asegúrate de importar el modelo User
-        empty_label="Seleccione un usuario"
+        queryset=User.objects.all().exclude(pk=1),
+        empty_label="Seleccione un usuario",
+        widget=forms.Select(attrs={'class': 'form-control'})
     )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            # Personalizar la clase de las etiquetas
+            field.label = field.label
